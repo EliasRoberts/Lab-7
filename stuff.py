422d7b9fc3ca3def83df85da57fee066d427aca8
EliasRoberts <eliasroberts99@gmail.com>
2014-10-14 14:08:41 -0500

    Lab7

---
+++ stuff.py
@@ -0,0 +1,14 @@
+totalolol = (0)
+teInput = int(raw_input())
+for ala in range (teInput):
+    print "Whatcha want, man!?"
+    tetotal = totalolol + teInput
+print tetotal
+
+total = []
+for x in range (teInput):
+    print "it's okay if it's in a _ way"
+    teInput = int (raw_input())
+    total.append (teInput)
+print sum(total)
+
